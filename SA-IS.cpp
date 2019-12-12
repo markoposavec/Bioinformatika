6baa8c707d1f4ddb1f106fa99ce48ff7ceff8a28
markoposavec <58392492+markoposavec@users.noreply.github.com>
2019-12-12 21:51:14 +0100

    Add files via upload
    sa-is up to the recursion

--- SA-IS.cpp
+++ SA-IS.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-void classify_characters(string s, vector<bool> &t) {
-	for (int i = s.length() - 1; i >= 0; i--) {
-		if (i == s.length() - 1) {
+void classify_characters(vector<int> s, vector<bool> &t) {
+	for (int i = s.size() - 1; i >= 0; i--) {
+		if (i == s.size() - 1) {
 			t.push_back(true);
 			continue;
 		}
@@ -24,8 +24,8 @@ void classify_characters(string s, vector<bool> &t) {
 	reverse(t.begin(), t.end());
 }
 
-void find_lms_substrings(string s, vector<bool> t, vector<int> &p1) {
-	for (int i = 1; i < s.length(); i++) {
+void find_lms_substrings(vector<int> s, vector<bool> t, vector<int> &p1) {
+	for (int i = 1; i < s.size(); i++) {
 		if ((t[i] == true && t[i - 1] == false)) {
 			p1.push_back(i);
 		}
@@ -49,21 +49,21 @@ void move_bk_pointers_to_start(map<char, int> mapa, map<char, int> &b) {
 	}
 }
 
-void induced_sort_lms_substrings(string s, vector<int> &sa, vector<int> &p1, vector<bool> t) {
+void induced_sort_lms_substrings(vector<int> s, vector<int> &sa, vector<int> &p1, vector<bool> t) {
 	map<char, int> b;
 	map<char, int> map;
-	for (long i = 0; i < s.length(); i++) {
+	for (long i = 0; i < s.size(); i++) {
 		map[s[i]]++;
 	}
 
-	for (int i = 0; i < s.length(); i++) {
+	for (int i = 0; i < s.size(); i++) {
 		sa.push_back(-1);
 	}
 	
 	move_bk_pointers_to_end(map, b);
 
 	int counter = 0;
-	for (int i = 0; i < s.length(); i++) {
+	for (int i = 0; i < s.size(); i++) {
 		if (p1[counter] == i) {
 			sa[b[s[i]]] = i;
 			b[s[i]]--;
@@ -97,8 +97,53 @@ void induced_sort_lms_substrings(string s, vector<int> &sa, vector<int> &p1, vec
 
 }
 
-int sa_is(string s, vector<int> &sa) {
-	int n = s.length();
+bool compare(vector<int> s, vector<bool> t, int last, int index) {
+	if (last == -1)
+		return false;
+	bool same = true;
+	for (int i = last, j = index; i < s.size() && j < s.size();) {
+		if (s[i] != s[j] || t[i] != t[j]) {
+			same = false;
+			break;
+		}
+		if (t[i] == t[j] && t[i] && i != last) break;
+		
+		i++;
+		j++;
+	}
+	return same;
+}
+
+pair<vector<int>, bool> lms_substring_naming(vector<int> s, vector<int> sa, vector<bool> t, vector<int> p1) {
+	int last = -1;
+	vector<int> s1;
+	for (int i = 0; i < p1.size(); i++) {
+		s1.push_back(-1);
+	}
+	bool unique = true;
+	int name_counter = 0;
+	for (int i = 0; i < sa.size(); i++) {
+		int index = sa[i];
+		for (int j = 0; j < p1.size(); j++) {
+			if (p1[j] == index) {
+				if (compare(s, t, last, index)) {
+					//potencijalno krivo hehe
+					unique = false;
+					s1[j] = name_counter - 1;
+					last = index;
+				} else {
+					s1[j] = name_counter++;
+					last = index;
+				}
+				break;
+			}
+		}
+	}
+	return make_pair(s1, unique);
+}
+
+int sa_is(vector<int> s, vector<int> &sa) {
+	int n = s.size();
 	vector<bool> t;
 	vector<int> p1;
 	vector<int> s1;
@@ -110,11 +155,27 @@ int sa_is(string s, vector<int> &sa) {
 	find_lms_substrings(s, t, p1);
 
 	induced_sort_lms_substrings(s, sa, p1, t);
+
+	pair<vector<int>, bool> substring_naming = lms_substring_naming(s, sa, t, p1);
+	bool unique = substring_naming.second;
+	s1 = substring_naming.first;
+
+	if (unique) {
+
+	} else {
+		sa_is(s1, sa);
+	}
+
 	return 0;
 }
 
 
 int main() {
+	vector<int> s;
 	vector<int> sa;
-	sa_is("ATTAGCGAGCG$", sa);
+	string temp = "ATTAGCGAGCG$";
+	for (int i = 0; i < temp.length(); i++) {
+		s.push_back(temp[i]);
+	}
+	sa_is(s, sa);
 }
